1d20e78206ae839968ddd1c50ac59ff8d80adbff
Rafael Simionato <rafaasimi@hotmail.com>
2022-01-27 15:03:58 -0300

    Alterado estrutura de pastas e adicionador recurso de carregamento.

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,11 +1,13 @@
 import { Box, Text, TextField, Image, Button } from '@skynexui/components';
 import React, { useEffect, useState } from 'react';
-import appConfig from '../config.json';
+import appConfig from '../../config.json';
 import { createClient } from '@supabase/supabase-js'
+
 
 export default function ChatPage() {
     const [message, setMessage] = useState('')
     const [messageList, setMessageList] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
 
     const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzI4NTkwNiwiZXhwIjoxOTU4ODYxOTA2fQ.ytgMF_N89xVP5rgPtv2eHlDmZG-tLcyqc3szrSYeb9c'
     const SUPABASE_URL = 'https://wnqdcabnlddyntengxyu.supabase.co'
@@ -19,6 +21,7 @@ export default function ChatPage() {
             .order('id', { ascending: false })
             .then(({ data }) => {
                 setMessageList(data)
+                setIsLoaded(true)
             })
     }, [])
 
@@ -58,7 +61,7 @@ export default function ChatPage() {
     function handleDeleteMessage(event) {
         const messageId = Number(event.target.dataset.id)
         console.log(messageId);
-        
+
         supabaseClient
             .from('messages')
             .delete()
@@ -75,106 +78,141 @@ export default function ChatPage() {
 
     }
 
-    return (
-        <Box
-            styleSheet={{
-                display: 'flex', alignItems: 'center', justifyContent: 'center',
-                backgroundColor: 'rgba(0,0,0,0.6)',
-                backgroundImage: 'url(https://wallpaperaccess.com/full/4858991.jpg)',
-                backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
-                color: appConfig.theme.colors.neutrals['000']
-            }}
-        >
-            <Box
-                styleSheet={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    flex: 1,
-                    boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-                    borderRadius: '5px',
-                    backgroundColor: 'rgba( 255, 255, 255, 0.21 )',
-                    height: '100%',
-                    maxWidth: '95%',
-                    maxHeight: '95vh',
-                    padding: '32px',
-                }}
-            >
-                <Header />
+    if (!isLoaded) {
+        return (
+            <>
+                <style global jsx>{`
+                    .loading-image {
+                        max-width: 200px;
+                        max-height: 200px;
+                        animation: rotation .5s linear infinite; 
+                    }
+
+                    @keyframes rotation {
+                        to {
+                            transform: rotate(360deg);
+                        }
+                    }
+                `}</style>
+
                 <Box
                     styleSheet={{
-                        position: 'relative',
-                        display: 'flex',
-                        flex: 1,
-                        height: '80%',
-                        backgroundColor: 'rgba( 0, 0, 0, 0.21 )',
-                        flexDirection: 'column',
-                        borderRadius: '5px',
-                        padding: '16px',
+                        display: 'flex', alignItems: 'center', justifyContent: 'center',
+                        backgroundColor: 'rgba(0,0,0,0.6)',
+                        backgroundImage: 'url(https://wallpaperaccess.com/full/4858991.jpg)',
+                        backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+                        color: appConfig.theme.colors.neutrals['000']
                     }}
                 >
 
-                    <MessageList messageList={messageList} handleDeleteMessage={handleDeleteMessage} />
-                    {/* Lista de mensagens:
-                    <ul>
-                        {messageList.map((messageItem) => {
-                            return (
-                                <li key={messageItem.id}>
-                                    {messageItem.user}: {messageItem.text}
-                                </li>
-                            )
-                        })}
-                    </ul> */}
+                    <img src="baby-yoda.png" className='loading-image' />
+                </Box>
+            </>
+        )
+    }
 
+    if (isLoaded) {
+        return (
+            <Box
+                styleSheet={{
+                    display: 'flex', alignItems: 'center', justifyContent: 'center',
+                    backgroundColor: 'rgba(0,0,0,0.6)',
+                    backgroundImage: 'url(https://wallpaperaccess.com/full/4858991.jpg)',
+                    backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+                    color: appConfig.theme.colors.neutrals['000']
+                }}
+            >
+                <Box
+                    styleSheet={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        flex: 1,
+                        boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                        borderRadius: '5px',
+                        backgroundColor: 'rgba( 255, 255, 255, 0.21 )',
+                        height: '100%',
+                        maxWidth: '95%',
+                        maxHeight: '95vh',
+                        padding: '32px',
+                    }}
+                >
+                    <Header />
                     <Box
-                        as="form"
                         styleSheet={{
+                            position: 'relative',
                             display: 'flex',
-                            alignItems: 'center',
+                            flex: 1,
+                            height: '80%',
+                            backgroundColor: 'rgba( 0, 0, 0, 0.21 )',
+                            flexDirection: 'column',
+                            borderRadius: '5px',
+                            padding: '16px',
                         }}
                     >
-                        <TextField
-                            value={message}
-                            onChange={(event) => {
-                                setMessage(event.target.value);
-                            }}
-                            onKeyPress={(event) => {
-                                if (event.key === 'Enter') {
-                                    event.preventDefault();
-                                    handleNewMessage(message);
-                                }
-                            }}
-                            placeholder="Insira sua mensagem aqui..."
-                            type="textarea"
+
+                        <MessageList messageList={messageList} handleDeleteMessage={handleDeleteMessage} />
+                        {/* Lista de mensagens:
+                        <ul>
+                            {messageList.map((messageItem) => {
+                                return (
+                                    <li key={messageItem.id}>
+                                        {messageItem.user}: {messageItem.text}
+                                    </li>
+                                )
+                            })}
+                        </ul> */}
+
+                        <Box
+                            as="form"
                             styleSheet={{
-                                width: '100%',
-                                border: '0',
-                                resize: 'none',
-                                borderRadius: '5px',
-                                padding: '6px 8px',
-                                backgroundColor: appConfig.theme.colors.neutrals[800],
-                                marginRight: '12px',
-                                color: appConfig.theme.colors.neutrals[200],
+                                display: 'flex',
+                                alignItems: 'center',
                             }}
-                        />
-                        <Button
-                            onClick={() => handleNewMessage(message)}
-                            label='Entrar'
-                            fullWidth
-                            styleSheet={{
-                                maxWidth: '100px',
-                            }}
-                            buttonColors={{
-                                contrastColor: appConfig.theme.colors.neutrals["000"],
-                                mainColor: appConfig.theme.colors.primary[500],
-                                mainColorLight: appConfig.theme.colors.primary[400],
-                                mainColorStrong: appConfig.theme.colors.primary[600],
-                            }}
-                        />
+                        >
+                            <TextField
+                                value={message}
+                                onChange={(event) => {
+                                    setMessage(event.target.value);
+                                }}
+                                onKeyPress={(event) => {
+                                    if (event.key === 'Enter') {
+                                        event.preventDefault();
+                                        handleNewMessage(message);
+                                    }
+                                }}
+                                placeholder="Insira sua mensagem aqui..."
+                                type="textarea"
+                                styleSheet={{
+                                    width: '100%',
+                                    border: '0',
+                                    resize: 'none',
+                                    borderRadius: '5px',
+                                    padding: '6px 8px',
+                                    backgroundColor: appConfig.theme.colors.neutrals[800],
+                                    marginRight: '12px',
+                                    color: appConfig.theme.colors.neutrals[200],
+                                }}
+                            />
+                            <Button
+                                onClick={() => handleNewMessage(message)}
+                                label='Entrar'
+                                fullWidth
+                                styleSheet={{
+                                    maxWidth: '100px',
+                                }}
+                                buttonColors={{
+                                    contrastColor: appConfig.theme.colors.neutrals["000"],
+                                    mainColor: appConfig.theme.colors.primary[500],
+                                    mainColorLight: appConfig.theme.colors.primary[400],
+                                    mainColorStrong: appConfig.theme.colors.primary[600],
+                                }}
+                            />
+                        </Box>
                     </Box>
                 </Box>
             </Box>
-        </Box>
-    )
+        )
+    }
 }
 
 function Header() {
